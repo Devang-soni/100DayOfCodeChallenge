bf022f0a178fbef535f3dad9d13701542db45a25
Devang vinod soni <127401975+Devang-soni@users.noreply.github.com>
2023-05-15 22:55:33 +0530

    Add files via upload
    Palindrome Number

---
+++ Day3.cpp
@@ -0,0 +1,17 @@
+
+//Given an integer x, return true if x is a palindrome , and false otherwise.
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        
+        int l=0, r= str.size()-1;
+        
+        while(1<=r){
+            if(str[l++] != str[r--])
+                return false;
+        }
+        return true;
+    }
+};
