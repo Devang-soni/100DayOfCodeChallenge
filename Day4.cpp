4bc846dc041126c28fa31de2303208a246842df4
Devang vinod soni <127401975+Devang-soni@users.noreply.github.com>
2023-05-16 17:13:58 +0530

    Update Day4.cpp

--- Day4.cpp
+++ Day4.cpp
@@ -1,3 +1,9 @@
+// Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+// The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+// Return the quotient after dividing dividend by divisor.
+
 #include <iostream>
 #include <limits>
 using namespace std;
