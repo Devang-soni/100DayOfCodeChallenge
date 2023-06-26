c61b8908787fd3fc63b44bc9b6ce9279c3848ea8
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-06-26 21:41:59 +0530

    Create Day23.cpp

---
+++ Day23.cpp
@@ -0,0 +1,47 @@
+//623. Add One Row to Tree
+class Solution {
+public:
+    
+    void insert(TreeNode* &node, int val, int d, int curDepth) {
+        
+        if(node == NULL)                        return;
+        
+        if(curDepth == d - 1) {
+            
+            // Here, we need to insert the node having value val
+            
+            // For left node
+            TreeNode* temp = node->left;
+            node->left = new TreeNode(val);
+            node->left->left = temp;
+            
+            // For right node
+            temp = node->right;
+            node->right = new TreeNode(val);
+            node->right->right = temp;
+        }
+        
+        else {
+            
+            insert(node->left, val, d, curDepth + 1);
+            insert(node->right, val, d, curDepth + 1);
+        }
+        
+        return;
+    }
+    
+    TreeNode* addOneRow(TreeNode* root, int val, int d) {
+    
+        if(d == 1) {
+            
+            TreeNode* node = new TreeNode(val);
+            node->left = root;
+            
+            return node;
+        }
+        
+        insert(root, val, d, 1);
+        return root;
+    }
+    
+};
