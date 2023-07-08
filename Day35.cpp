da1bb4b0da6f3e08337b966e5b9ee01cae9d4b45
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-07-08 18:25:41 +0530

    Create Day35.cpp

---
+++ Day35.cpp
@@ -0,0 +1,15 @@
+//26. Remove Duplicates from Sorted Array
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int ans=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[ans]){
+                ans++;
+                nums[ans]=nums[i];
+            }
+        }
+        return ans+1;
+    }
+};
