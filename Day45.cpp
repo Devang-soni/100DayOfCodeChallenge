b879a7411dbc582573525bc50261ea6a90e79e4d
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-07-18 22:30:12 +0530

    Create Day45.cpp

---
+++ Day45.cpp
@@ -0,0 +1,15 @@
+//1668. Maximum Repeating Substring
+class Solution {
+public:
+	int maxRepeating(string sequence, string word) {
+		int k = 0;
+		string temp = word;
+
+		while(sequence.find(temp) != string::npos){
+			temp += word;
+			k++;
+		}
+
+		return k;
+	}
+};
