ab6e031f5eed65305fcad917a50fa0c6f5ea5cb8
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-07-22 23:15:47 +0530

    Create Day49.cpp

---
+++ Day49.cpp
@@ -0,0 +1,19 @@
+//1780. Check if Number is a Sum of Powers of Three
+
+class Solution
+{
+public:
+    bool checkPowersOfThree(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+        if (n % 3 == 2)
+        {
+            return false;
+        }
+
+        return checkPowersOfThree(n / 3);
+    }
+};
