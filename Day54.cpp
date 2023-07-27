4ffbf36e7429854c626a831c20b22e272a42c689
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-07-27 18:49:35 +0530

    Create Day54.cpp

---
+++ Day54.cpp
@@ -0,0 +1,12 @@
+//69. Sqrt(x)
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long result = x;
+        while (result * result > x) {
+            result = (result + (x/result))/2;
+        }
+        return (int)result;
+    }
+};
