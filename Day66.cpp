9ac0ba729b65f13ad565f38f66817b5128ed0e10
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-08-08 16:26:56 +0530

    Create Day66.cpp

---
+++ Day66.cpp
@@ -0,0 +1,15 @@
+//268. Missing Number
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=(n * (n+1))/2;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            sum=sum-nums[i];
+        }
+        return sum;
+        
+    }
+};
