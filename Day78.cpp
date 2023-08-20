e645427829f6b9cfdf8330e690150f1d920740d0
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-08-20 16:05:03 +0530

    Create Day78.cpp

---
+++ Day78.cpp
@@ -0,0 +1,17 @@
+//215. Kth Largest Element in an Array
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int m = nums.size();
+        int d = m - k;
+        int ans;
+        for(int i =0; i < nums.size(); i++){
+            if(i == d){
+                ans = nums[d];
+            }
+        }
+        return ans;
+    }
+};
