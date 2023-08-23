c8399e279ddf2f67422917faa55c05fba1e3ce1c
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-08-23 11:58:17 +0530

    Create Day82.cpp

---
+++ Day82.cpp
@@ -0,0 +1,14 @@
+//1528. Shuffle String
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+    string result(s.size(), ' ');  
+    
+    for(int i = 0; i < s.size(); ++i) {
+        result[indices[i]] = s[i];
+    }
+    
+    return result;
+    }
+};
