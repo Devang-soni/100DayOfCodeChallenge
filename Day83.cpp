8ebbe261b3394228bba88a62bf129775342b142a
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-08-24 21:40:14 +0530

    Create Day83.cpp

---
+++ Day83.cpp
@@ -0,0 +1,16 @@
+//1221. Split a String in Balanced Strings
+
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count = 0 , l = 0, r = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == 'L' ) l++;
+            if(s[i] == 'R') r++;
+            if( l == r)
+                count++;
+        }
+        return count;
+    }
+};
