eb2edde6b693cb32f3abdd160c1126adbb0c783f
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-08-27 19:57:23 +0530

    Create Day87.cpp

---
+++ Day87.cpp
@@ -0,0 +1,18 @@
+//2544. Alternating Digit Sum
+
+class Solution {
+public:
+    int alternateDigitSum(int n){
+        string str = to_string(n);
+        int sum = 0;
+
+        for(size_t i = 0; i < str.length(); i++) {
+            if(i % 2 == 0) {
+                sum += str[i] - '0';
+            } else {
+                sum -= str[i] - '0';
+            }
+        }
+        return sum;
+    }
+};
