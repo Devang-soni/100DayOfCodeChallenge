93ad6a537e13bb8bc88049206c95f0f1ca068559
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-09-01 17:32:14 +0530

    Create Day93.cpp

---
+++ Day93.cpp
@@ -0,0 +1,18 @@
+//2154. Keep Multiplying Found Values by Two
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        int n = 1;
+        for(int i = 0; i<n;++i)
+        {
+            if(find(nums.begin(),nums.end(),original) != nums.end())
+            {
+                original *= 2;
+                n += 1; 
+
+            }
+        }
+        return original;
+    }
+};
