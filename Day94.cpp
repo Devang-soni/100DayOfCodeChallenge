50ebf4cf79099881115d749c13fb833a2633e9b5
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2023-09-02 11:35:08 +0530

    Create Day94.cpp

---
+++ Day94.cpp
@@ -0,0 +1,14 @@
+//169. Majority Element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        
+        sort(nums.begin(), nums.end());
+        
+        return nums[nums.size() / 2];
+    }
+};
